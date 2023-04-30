df64d73ee46cf2995c699747cf1e9ac5e0067423
Wilson Cusack <cusackw@gmail.com>
2023-04-30 14:44:42 -0400

    lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,13 +46,21 @@ export default function Home({
 
     const allVotes = await fetchProposalVotes(proposal.id);
     const forVotes = allVotes
-    .filter((vote: VoteWithLikes) => vote.support === true)
-    .sort((a: VoteWithLikes, b: VoteWithLikes) => (b.nounHolderLikes + b.nonNounHolderLikes) - (a.nounHolderLikes + a.nonNounHolderLikes));
+      .filter((vote: VoteWithLikes) => vote.support === true)
+      .sort(
+        (a: VoteWithLikes, b: VoteWithLikes) =>
+          b.nounHolderLikes +
+          b.nonNounHolderLikes -
+          (a.nounHolderLikes + a.nonNounHolderLikes)
+      );
     const againstVotes = allVotes
-    .filter(
-      (vote: Vote) => vote.support === false
-    )
-    .sort((a: VoteWithLikes, b: VoteWithLikes) => (b.nounHolderLikes + b.nonNounHolderLikes) - (a.nounHolderLikes + a.nonNounHolderLikes));
+      .filter((vote: Vote) => vote.support === false)
+      .sort(
+        (a: VoteWithLikes, b: VoteWithLikes) =>
+          b.nounHolderLikes +
+          b.nonNounHolderLikes -
+          (a.nounHolderLikes + a.nonNounHolderLikes)
+      );
 
     setForVotes(forVotes);
     setAgainstVotes(againstVotes);
