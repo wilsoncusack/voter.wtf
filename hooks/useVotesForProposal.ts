5a8377c41244afef0d96e2090a68e4db3ce1395a
Wilson Cusack <cusackw@gmail.com>
2023-05-02 21:53:28 -0400

    vote likes sort fix

--- hooks/useVotesForProposal.ts
+++ hooks/useVotesForProposal.ts
@@ -9,14 +9,23 @@ export function useVotesForProposal(id: string) {
   );
 
   const forVotes = useMemo(
-    () => data?.filter((vote: Vote) => vote.support),
+    () => data?.filter((vote: Vote) => vote.support).sort(sortOnLikes),
     [data]
   );
 
   const againstVotes = useMemo(
-    () => data?.filter((vote: Vote) => !vote.support),
+    () => data?.filter((vote: Vote) => !vote.support).sort(sortOnLikes),
     [data]
   );
 
   return { forVotes, againstVotes, ...rest };
 }
+
+const sortOnLikes = (a: VoteWithLikes, b: VoteWithLikes) => {
+  if (!a.nounHolderLikes || !b.nounHolderLikes) return 0;
+  return (
+    b.nonNounHolderLikes.length +
+    b.nounHolderLikes.length -
+    (a.nonNounHolderLikes.length + a.nounHolderLikes.length)
+  );
+};
