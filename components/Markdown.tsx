f878392e8c53b8e4399ca102f9c4b24e9257cb3d
Wilson Cusack <cusackw@gmail.com>
2023-06-22 09:33:19 -0400

    improve markdown

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -57,6 +57,13 @@ export function Markdown({ text }: { text: string }) {
               </code>
             );
           },
+          ul: ({ node, ...props }) => (
+            <ul className="list-disc ml-5" {...props} />
+          ),
+          ol: ({ node, ...props }) => (
+            <ol className="list-decimal ml-5" {...props} />
+          ),
+          li: ({ node, ...props }) => <li className="mb-1" {...props} />,
         }}
       >
         {text}
